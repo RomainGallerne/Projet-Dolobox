2576f4f61b7e3b0f870b57542af239b08a6e5985
MorganNavel <navelmorgan34@gmail.com>
2024-04-17 14:34:55 +0200

    TaskManager & Signleton

---
+++ src/Singleton.py
@@ -0,0 +1,13 @@
+from functools import wraps
+
+
+def Singleton(cls):
+    instances = {}
+
+    @wraps(cls)
+    def get_instance(*args, **kwargs):
+        if cls not in instances:
+            instances[cls] = cls(*args, **kwargs)
+        return instances[cls]
+
+    return get_instance
