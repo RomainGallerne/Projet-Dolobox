d67137c11ffd1265596ba9077697e78da7ac820e
Morgan Navel <navelmorgan34@gmail.com>
2024-05-02 18:51:10 +0200

    api request

--- src/main.py
+++ src/main.py
@@ -1,10 +1,20 @@
-from machine import ADC, Pin
-from time import sleep
+from machine import Pin, PWM, ADC, RTC
+import network
 import uasyncio
+from time import sleep
+import urequests
 
-btn = Pin(23, Pin.IN, Pin.PULL_UP)
-slider = ADC(Pin(36))
-slider.atten(ADC.ATTN_11DB)
+
+def get_current_datetime():
+    rtc = RTC()
+    year, month, day, weekday, hour, minute, second, _ = rtc.datetime()
+    # Ajouter un zéro devant les valeurs inférieures à 10
+    day_str = "0{}".format(day) if day < 10 else str(day)
+    month_str = "0{}".format(month) if month < 10 else str(month)
+    hour_str = "0{}".format(hour) if hour < 10 else str(hour)
+    minute_str = "0{}".format(minute) if minute < 10 else str(minute)
+    second_str = "0{}".format(second) if second < 10 else str(second)
+    return "{}-{}-{} {}:{}:{}".format(day_str, month_str, year, hour_str, minute_str, second_str)
 
 def do_connect():
     import network
@@ -12,10 +22,11 @@ def do_connect():
     wlan.active(True)
     if not wlan.isconnected():
         print('connecting to network...')
-        wlan.connect('ssid', 'key') # Id et mot de passe wifi
+        wlan.connect("Morgan's Galaxy A33 5G", 'jxvz9984')
         while not wlan.isconnected():
             pass
     print('network config:', wlan.ifconfig())
+    
 
 def Singleton(cls):
     instances = {}
@@ -31,39 +42,69 @@ def Singleton(cls):
 @Singleton
 class ApiService:
     def __init__(self):
-        load_dotenv()
-        self.base_url = os.getenv("API_URL")
+        # Chargez les paramètres nécessaires
+        self.base_url = "https://eriospainapi.onrender.com"
         
     async def login(self, username, password):
         url = f"{self.base_url}/api/login"
         headers = {
             "Content-Type": "application/x-www-form-urlencoded"
         }
-        payload = f"username={username}&password={password}"
-
-        response = requests.post(url, headers=headers, data=payload)
-        if response.ok:
+        payload = "username={}&password={}".format(username, password)
+        try:
+            response = urequests.post(url, headers=headers, data=payload)
             return response.json()
-        return None
+        except Exception as e:
+            print("Error in login request:", e)
+            return None
+
+    async def getUsers(self, token):
+        url = f"{self.base_url}/api/users"
+        headers = {
+            "Authorization": f"Bearer {token}",
+            "Content-Type": "application/json"
+        }
+        try:
+            response = urequests.get(url, headers=headers)
+            return response.json()
+        except Exception as e:
+            print("Error in getUsers request:", e)
+            return None
 
     async def add_streams(self, patient_id, records, token):
         headers = {
             "Authorization": f"Bearer {token}",
-            "Content-Type": "application/json"  # Utilisez 'application/json' pour envoyer des données JSON
+            "Content-Type": "application/json"
         }
         url = f"{self.base_url}/api/patient/{patient_id}/streams"
-        payload = {"records": records}  # Construisez la charge utile comme un objet JSON
-        response = requests.post(url, headers=headers, json=payload)  # Utilisez 'json=payload' pour envoyer des données JSON
-        if response.ok:
+        payload = {"records": records}
+        try:
+            response = urequests.post(url, headers=headers, json=payload)
             return response.json()
-        return None
+        except Exception as e:
+            print("Error in add_streams request:", e)
+            return None
 
-async def press():
-    while True:
-        if(btn.value() == 0):
-            print("Mesure de douleur: "+str(slider.read()))
-            sleep(1)
         
+async def send_data():
+    value = slider.read()
+    date = get_current_datetime()
+    records = [{"level":value,"evaluation_date":date}]
+    api = ApiService()
+    print(records)
+    token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTcxNDY2Nzc2MywiZXhwIjoxNzE0NjcxMzYzfQ.bcFqOUs2YNcEeXM-vFNmjiXIBkXSnsnEzKqe5iD58Ag"
+    response = await api.add_streams(1,records,token)
+    print(response)
+def pressed(btn):
+    print("pressed")
+    task = uasyncio.create_task(send_data())
+    # Attendre que la tâche soit terminée
+    loop = uasyncio.get_event_loop()
+    loop.run_until_complete(task)
+    
+pin_button.irq(trigger=Pin.IRQ_FALLING, handler=pressed)
 
+do_connect()
 
-uasyncio.run(press())
+while True:
+    pass
