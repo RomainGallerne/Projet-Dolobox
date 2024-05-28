88bc18b808d67fe5e2a581abf7914a8a5989c10b
Morgan Navel <80525825+MorganNavel@users.noreply.github.com>
2024-05-28 08:10:27 +0200

    Update boot.py

--- dolobox/src/boot.py
+++ dolobox/src/boot.py
@@ -1,6 +1,19 @@
-# boot.py -- Fichier de configuration exécuté à chaque démarrage
+# This file is executed on every boot (including wake-boot from deepsleep)
+import network
+from config import Config
 
+def do_connect():
+    import network
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('connecting to network...')
+        wlan.connect(Config.get_config("WIFI_NAME"),Config.get_config("WIFI_PASSWORD"))
+        while not wlan.isconnected():
+            pass
+    print('network config:', wlan.ifconfig())
 try:
+    do_connect()
     import main
 except ImportError:
     print("main.py not found")
